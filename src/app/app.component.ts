a14229efcde35884565a48195ac88fa40f3274c5
Jugal Munganga <jugsonmunganga391@gmail.com>
2022-06-20 14:35:10 +0100

    Fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { GarageService } from './garage.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ GarageService ] //providers qui est un tableau, il va utilisé la dependence de Garage service
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'garageAppFront';
+  cars: any
+
+  constructor(private garageService: GarageService){
+
+  }
+
+  ngOnInit(): void {
+      console.log('On init....')
+      this.garageService.getCars().subscribe((datas) => {this.cars = datas ;
+      })
+  }
 }
